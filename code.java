7e93cbafb6609b4f1b1d9c9fa3133d6e289f1b24
prince choudhary <prince.star0798@gmail.com>
2025-05-11 12:58:41 +0530

    Initial commit

---
+++ code.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class SudokuSolver {
+
+    private static final int SIZE = 9;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = 0; 
+                            }
+                        }
+                    }
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+    
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int boxRowStart = row - row % 3;
+        int boxColStart = col - col % 3;
+
+        for (int i = boxRowStart; i < boxRowStart + 3; i++) {
+            for (int j = boxColStart; j < boxColStart + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("------+-------+------");
+            }
+            for (int col = 0; col < SIZE; col++) {
+                if (col % 3 == 0 && col != 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
